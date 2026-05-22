998bf50e246719ea4be3537f6f4b71aa3a0cab32
panishthaa <panishthaa@gmail.com>
2026-05-22 16:13:41 +0530

    connect frontend to deployed backend

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
-
+API_URL = "https://web-production-9023ad.up.railway.app"
 st.set_page_config(
     page_title="AI Banking Support Chatbot",
     page_icon="💳",
